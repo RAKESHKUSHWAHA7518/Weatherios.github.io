b1b63f3c82f03156682b56ca883349093deccb6b
Rakesh kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2024-12-03 10:26:32 +0530

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,4 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+ 
 
 // 'use strict';
 // const api_key = "7775811b5bc22ad000d8cbdc6e86bda0";
