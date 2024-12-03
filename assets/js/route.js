144be974b176971ed442fcb2bf3e62505e3f54ee
Rakesh kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2024-12-03 10:25:45 +0530

    Update route.js

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,9 +1,4 @@
-/**
- * @license MIT
- * @fileoverview Menage all routes
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+ 
 
 
 'use strict';
