8a6ee1d7f0d082981347d1098ec54832371b1c98
Rakesh kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2024-12-03 10:26:07 +0530

    Update module.js

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,10 +1,4 @@
-/**
- * @license MIT
- * @fileoverview All module functions
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
+ 
 
 
 
